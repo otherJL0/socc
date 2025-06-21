a71a920e5863d03c8fce45c010a4ff86079d8ec5
Jonathan Lopez <jonathanglopez@gmail.com>
2025-06-21 11:56:25 -0400

    Adding basic prompt

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,21 @@
-let () = print_endline "Hello, World!"
+let run = print_endline
+
+let run_prompt =
+ fun _ ->
+  while true do
+    print_string "> ";
+    let input = read_line () in
+    run input
+  done
+
+let run_file input_file =
+  print_endline input_file;
+  exit 0
+
+let () =
+  match Array.length Sys.argv with
+  | 0 | 1 -> run_prompt ()
+  | 2 -> run_file (Array.get Sys.argv 1)
+  | _ ->
+      print_endline "Usage: socc [filename]";
+      exit 64
